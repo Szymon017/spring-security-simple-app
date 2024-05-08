9346be6826928a9195a31cee43788eaab8c37eb6
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2024-05-08 20:21:07 +0200

    first commit

---
+++ src/main/java/com/example/security/model/MyUserDetailService.java
@@ -0,0 +1,38 @@
+package com.example.security.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    MyUserRepository myUserRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<MyUser> user = myUserRepository.findByUsername(username);
+        var userObj = user.get();
+        if(user.isPresent()){
+            return User.builder()
+                    .username(userObj.getUsername())
+                    .password(userObj.getPassword())
+                    .roles(userObj.getRole())
+                    .build();
+        } else {
+            throw new UsernameNotFoundException(username);
+        }
+    }
+
+    private String[] getRoles(MyUser user) {
+        if(user.getRole() == null){
+            return new String[] {"USER"};
+        }
+        return user.getRole().split(",");
+    }
+}
